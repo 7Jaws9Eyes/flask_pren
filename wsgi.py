39e0593c9af8b0ab27697629fa6ca66f8b47736b
Corsin Kirchhofer <cxultimate@gmail.com>
2022-04-15 17:38:08 +0200

    using correct call name for flask

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app.main import socketio, app
 
 print(f"WSGI Called, name: {__name__}")
 
-if __name__ == "__main__" or __name__ == 'app':
+if __name__ == "__main__" or __name__ == 'wsgi':
     print('wsgi main called')
     app.run()
     # socketio.run(app)
