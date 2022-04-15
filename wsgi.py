d783661f08e87733f476bc15cfac98cb02f8242c
Corsin Kirchhofer <cxultimate@gmail.com>
2022-04-15 17:26:47 +0200

    im confused about what procfile config is being called

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ print(f"WSGI Called, name: {__name__}")
 
 if __name__ == "__main__" or __name__ == 'app':
     print('wsgi main called')
-    # socketio.run(app)
     app.run()
+    # socketio.run(app)
