1ae7e5a80aa13b6448c827a06f1132c50de392a0
Corsin Kirchhofer <cxultimate@gmail.com>
2022-04-15 16:53:15 +0200

    further debugging, added app as name for main

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from app.main import socketio, app
 
 print(f"WSGI Called, name: {__name__}")
-if __name__ == "__main__":
+
+if __name__ == "__main__" or __name__ == 'app':
+    print('wsgi main called')
     # socketio.run(app)
     app.run()
