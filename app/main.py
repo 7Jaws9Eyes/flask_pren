8be60c20a15418d697fccf064123b6f774f77fd1
Corsin Kirchhofer <cxultimate@gmail.com>
2022-05-07 23:06:45 +0200

    updated routing and added timer

--- app/main.py
+++ app/main.py
@@ -1,18 +1,20 @@
+import asyncio
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
-import random
 import json
+import time
 
 app = Flask(__name__, static_url_path='', static_folder='build')
 cors = CORS(app, resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app, cors_allowed_origins='*')
-
+start_time = time.time()
+stop_time = time.time()
+continue_time = False
 
 @app.route("/")
 def init():
-    # print("handle called")
     return app.send_static_file('index.html')
 
 
@@ -22,30 +24,8 @@ def run():
                 "name": "status",
                 "status": "offline"
             }
-    socketio.emit('status_request')
+    # socketio.emit('status_request')
     return json.dumps(data)
-
-
-def send_test_data(sensor_type, message):
-    for x in range(10):
-        data = {
-            "name": sensor_type,
-            "message": x
-        }
-        # print(f"sending test_data to: {sensor_type} Data: {data['message']}")
-        # print(f'test data json: {json.dumps({"data": data})}')
-        socketio.emit(sensor_type, json.dumps({"data": data}))
-        x = x + 1
-
-
-def update_sensor_data(sensor_type, message):
-    data = {
-        "name": sensor_type,
-        "message": message
-    }
-    print(f"Updating sensor data type: {sensor_type} Data: {data['message']}")
-    socketio.emit(sensor_type, json.dumps({"data": data}))
-
 
 @socketio.event
 def change_speed(data):
@@ -53,51 +33,53 @@ def change_speed(data):
 
 
 @socketio.event
-def request_test_data():
-    # print('test data being requested')
-    send_test_data('speed', 300)
-
-
-@socketio.event
 def event(data):
-    # print(f'event from client received {data}')
-    socketio.emit('event', json.dumps(data))
+    socketio.emit('event', data)
 
+
+# @socketio.event
+# def start_timer():
+#     print(f'timer started')
+#     global start_time
+#     global continue_time
+#     start_time = time.time()
+#     continue_time = True
+#     asyncio.run(ud())
+#     socketio.emit('start_time', start_time)
+
+#
+# @socketio.event
+# def stop_timer():
+#     global stop_time
+#     global start_time
+#     global continue_time
+#     stop_time = time.time()
+#     continue_time = False
+#     print('stopped timer')
+#     socketio.emit('final_time', stop_time-start_time)
+#
+#
+# async def ud():
+#     global continue_time
+#     print('starting ud')
+#     while continue_time:
+#         print('still goin')
+#         await update_time()
+#
+#
+# async def update_time():
+#     global start_time
+#     print('going to sleep')
+#     await asyncio.sleep(1)
+#     t = time.time() - start_time
+#     print(f'woke up in a new bugatti {t}')
+#     socketio.emit('present_time', t)
+#
 
 @socketio.event
-def speed(data):
-    # print(f'speed from client received {data}')
-    socketio.emit('speed', json.dumps(data))
-
-
-@socketio.event
-def voltage_print(data):
-    # print(f'voltage_print from client received {data}')
-    socketio.emit('voltage_print', json.dumps(data))
-
-
-@socketio.event
-def coils(data):
-    # print(f'coils from client received {data}')
-    socketio.emit('coils', json.dumps(data))
-
-
-@socketio.event
-def acceleration(data):
-    # print(f'acceleration from client received {data}')
-    socketio.emit('acceleration', json.dumps(data))
-
-
-@socketio.event
-def voltage_motor(data):
-    # print(f'voltage_motor from client received {data}')
-    socketio.emit('voltage_motor', json.dumps(data))\
-
-
-@socketio.event
-def plant_data(data):
-    print(f'plant data from client received {data}')
-    socketio.emit('plant_data', json.dumps(data))
+def sensor_update(data):
+    print(f'sensor update from client received {data}')
+    socketio.emit(data['name'], data['message'])
 
 
 @socketio.on('*')
